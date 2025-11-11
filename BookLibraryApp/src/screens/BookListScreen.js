58d08a48c4d6cb657f1ac1322bca5565aaae2c7d
HTrieuhehe <97211030+HTrieuhehe@users.noreply.github.com>
2025-11-10 19:47:18 -0500

    Update: Add basic code

---
+++ BookLibraryApp/src/screens/BookListScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function BookListScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Book List Screen</Text>
+    </View>
+  );
+}
