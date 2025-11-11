5b211ccc4ef654f73f2097d27a3d6b947e04f9b6
HTrieuhehe <97211030+HTrieuhehe@users.noreply.github.com>
2025-11-10 19:44:51 -0500

    Add: remove error when build

---
+++ BookLibraryApp/src/screens/BorrowedBooksScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function BorrowedBooksScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Borrowed Books Screen</Text>
+    </View>
+  );
+}
